1145e8e1be494d63b89b6509c4497f1109acaea8
Katalam <bruno.goerss@hyperlink.eu>
2023-01-22 22:06:04 +0100

    Moving functionality to a class

---
+++ src/Actions/DecodeJsonStructure.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Hyperlink\JsonStructure\Actions;
+
+class DecodeJsonStructure
+{
+    public function __construct(private readonly array $array)
+    {
+    }
+
+    public function __invoke(): string
+    {
+        return '['.PHP_EOL.$this->implode_recursive(',', $this->array_keys_r($this->array)).PHP_EOL.']'.PHP_EOL;
+    }
+
+    private function implode_recursive(string $separator, array $array, int $indent = 1): string
+    {
+        $string = '';
+        foreach ($array as $i => $a) {
+            if (is_array($a)) {
+                $string .= (str_repeat('    ', $indent))
+                    .'\''
+                    .$i
+                    .'\''
+                    .' => ['
+                    .PHP_EOL
+                    .$this->implode_recursive($separator, $a, $indent + 1)
+                    .PHP_EOL
+                    .(str_repeat('    ', $indent))
+                    .']';
+                $array1 = array_keys($array);
+                if ($i !== end($array1)) {
+                    $string .= $separator.PHP_EOL;
+                }
+            } else {
+                $string .= (str_repeat('    ', $indent)).'\''.$a.'\'';
+                if ($i < count($array) - 1) {
+                    $string .= $separator.PHP_EOL;
+                }
+            }
+        }
+
+        return $string;
+    }
+
+    private function is_array_of_arrays($array): bool
+    {
+        if (! is_array($array)) {
+            return false;
+        }
+        foreach ($array as $item) {
+            if (! is_array($item)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private function array_keys_r($array): array
+    {
+        $keys = array_keys($array);
+        $keys_array = [];
+
+        foreach ($array as $idx => $i) {
+            if ($this->is_array_of_arrays($i)) {
+                if ($index = array_search($idx, $keys, true)) {
+                    unset($keys[$index]);
+                }
+                $keys_array[$idx] = ['*' => $this->array_keys_r(array_values($i)[0])];
+
+                continue;
+            }
+            if (is_array($i)) {
+                if ($index = array_search($idx, $keys, true)) {
+                    unset($keys[$index]);
+                }
+                $keys_array[$idx] = $this->array_keys_r($i);
+            }
+        }
+
+        return array_merge(array_values($keys), $keys_array);
+    }
+}
--- src/Commands/JsonStructureCommand.php
+++ src/Commands/JsonStructureCommand.php
@@ -2,6 +2,7 @@
 
 namespace Hyperlink\JsonStructure\Commands;
 
+use Hyperlink\JsonStructure\Actions\DecodeJsonStructure;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
@@ -20,7 +21,6 @@ class JsonStructureCommand extends Command
 
 //        if ($this->argument('endpoint')) {
         $this->info('Getting json structure from endpoint...');
-
         $response = Http::get(route($this->argument('endpoint')));
 
         if ($response->failed()) {
@@ -44,7 +44,7 @@ class JsonStructureCommand extends Command
         }
 
         $this->info('Attempting to decode json structure...');
-        $result = '['.PHP_EOL.$this->implode_recursive(',', $this->array_keys_r($json)).PHP_EOL.']'.PHP_EOL;
+        $result = call_user_func(new DecodeJsonStructure($json));
         $this->comment('Decoding json structure successful');
 
         $this->info('Writing result to file...');
@@ -52,74 +52,5 @@ class JsonStructureCommand extends Command
         $this->comment('Writing result to file successful');
 
         return self::SUCCESS;
-    }
-
-    private function array_keys_r($array): array
-    {
-        $keys = array_keys($array);
-        $keys_array = [];
-
-        foreach ($array as $idx => $i) {
-            if ($this->is_array_of_arrays($i)) {
-                if ($index = array_search($idx, $keys, true)) {
-                    unset($keys[$index]);
-                }
-                $keys_array[$idx] = ['*' => $this->array_keys_r(array_values($i)[0])];
-
-                continue;
-            }
-            if (is_array($i)) {
-                if ($index = array_search($idx, $keys, true)) {
-                    unset($keys[$index]);
-                }
-                $keys_array[$idx] = $this->array_keys_r($i);
-            }
-        }
-
-        return array_merge(array_values($keys), $keys_array);
-    }
-
-    private function implode_recursive(string $separator, array $array, int $indent = 1): string
-    {
-        $string = '';
-        foreach ($array as $i => $a) {
-            if (is_array($a)) {
-                $string .= (str_repeat('    ', $indent))
-                    .'\''
-                    .$i
-                    .'\''
-                    .' => ['
-                    .PHP_EOL
-                    .$this->implode_recursive($separator, $a, $indent + 1)
-                    .PHP_EOL
-                    .(str_repeat('    ', $indent))
-                    .']';
-                $array1 = array_keys($array);
-                if ($i !== end($array1)) {
-                    $string .= $separator.PHP_EOL;
-                }
-            } else {
-                $string .= (str_repeat('    ', $indent)).'\''.$a.'\'';
-                if ($i < count($array) - 1) {
-                    $string .= $separator.PHP_EOL;
-                }
-            }
-        }
-
-        return $string;
-    }
-
-    private function is_array_of_arrays($array): bool
-    {
-        if (! is_array($array)) {
-            return false;
-        }
-        foreach ($array as $item) {
-            if (! is_array($item)) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
